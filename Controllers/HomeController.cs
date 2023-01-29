8191a6a7bfd3c43283dcea0d5e96639986564576
wenbin-98 <75748104+wenbin-98@users.noreply.github.com>
2023-01-29 13:13:01 +0800

    hi

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,23 +1,70 @@
 ﻿using FSMS_asp.net.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using FSMS_asp.net.Data;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace FSMS_asp.net.Controllers
 {
-    [Authorize]
+    //[Authorize]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ILogger<HomeController> _logger; 
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> IndexAsync()
         {
-            return View();
+            DateTime date = DateTime.Now;
+            var firstDayOfMonth = new DateTime(date.Year, date.Month, 1);
+            var lastDayOfMonth = firstDayOfMonth.AddMonths(1).AddDays(-1);
+            List<Chart> data = new List<Chart>();
+
+            for (var i = 5; i >= 0; i--)
+            {
+                var temp_first_day = firstDayOfMonth.AddMonths(-i);
+                var temp_last_day = lastDayOfMonth.AddMonths(-i);
+                //get all data of invoice
+                var models = await _context.InvoicesModel.Where(x => x.Date < temp_last_day && x.Date > temp_first_day).ToListAsync();
+
+                if (models.Any())
+                {
+                    decimal sum = 0;
+                    foreach (var item in models)
+                    {
+                        sum += item.TotalAmount;
+                    }
+
+                    Chart temp = new Chart()
+                    {
+                        Year = temp_first_day.ToString("MMMM yyyy"),
+                        Sales = sum
+                    };
+                    data.Add(temp);
+                } else
+                {
+                    Chart temp = new Chart()
+                    {
+                        Year = temp_first_day.ToString("MMMM yyyy"),
+                        Sales = 0
+                    };
+                    data.Add(temp);
+                }
+                
+            }
+            ViewBag.data = JsonSerializer.Serialize(data);
+            //return view with data of invoice
+            return _context.InvoicesModel != null ?
+                        View(data) :
+                        Problem("Entity set 'ApplicationDbContext.InvoicesModel'  is null.");
             //return Redirect("salesreports/index");
         }
 
---
+++ Models/Chart.cs
@@ -0,0 +1,8 @@
+﻿namespace FSMS_asp.net.Models
+{
+    public class Chart
+    {
+        public string Year { get; set; }
+        public decimal Sales { get; set; }
+    }
+}
